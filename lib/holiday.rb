d2a887063d09da337736448f056dc8998bd100ae
Certil Remy <certil@noukod.com>
2018-10-01 19:34:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,5 +68,5 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+[holiday_hash[:summer].keys[0], holiday_hash[:spring].keys[0]]
 end
